8954f0520fa9333bb8f8d4c842e9bcb5dcd78dbf
Brandon Randall <br.mhcexchange@gmail.com>
2017-09-25 14:04:18 -0600

    passes stations create spec

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -8,6 +8,16 @@ class BikeShareApp < Sinatra::Base
     erb :"/stations/index"
   end
 
+  get '/stations/new' do
+    @station = Station.new
+    erb :"/stations/new"
+  end
+
+  post '/stations' do
+    @station = Station.create(params[:station])
+    redirect "/stations/#{@station.id}"
+  end
+  
   get '/stations/:id' do
     @station = Station.find(params[:id])
     erb :"/stations/show"
