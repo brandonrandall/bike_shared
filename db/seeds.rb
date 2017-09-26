f79e456a52f8cdf68d449e645109cea162ce484a
Brandon Randall <br.mhcexchange@gmail.com>
2017-09-26 14:05:36 -0600

    properly formats installation date

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 require 'csv'
-require '/.app/models/station.rb'
+require './app/models/station.rb'
+require 'pry'
+require 'Date'
 
-CSV.foreach('db/csv/bike_stations.csv', headers: true) do |row|
+CSV.foreach('db/csv/bike_stations.csv', :headers => true) do |row|
   Station.create!({
     name: row["name"],
     dock_count: row["dock_count"],
     city: row["city"],
-    installation_date: row["installation_date"]})
+    installation_date: DateTime.strptime(row["installation_date"], "%m/%d/%Y")})
 end
