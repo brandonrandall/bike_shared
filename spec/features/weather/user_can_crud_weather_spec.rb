64f709c2bf3a371b65bb93474b66e135ec9ee4f8
Brandon Randall <br.mhcexchange@gmail.com>
2017-10-03 14:48:49 -0600

    adds background images

--- spec/features/weather/user_can_crud_weather_spec.rb
+++ spec/features/weather/user_can_crud_weather_spec.rb
@@ -2,24 +2,24 @@ require_relative '../../spec_helper'
 
 describe "when visiting conditions page" do
   it "a user can create a weather condition" do
-
     visit '/conditions'
     click_on("Add a Condition")
-
-    fill_in('station[name]', with: "Red Light District")
-    fill_in("Date",  with: Date.today)
-    fill_in("max_temp", with: 100)
-    fill_in "min_temp", with: 22
-    fill_in "man_humid", with: (100+22)/2
-    fill_in "mean_visibility", with: 3
-    fill_in "mean_wind_speed", with: 35
-    fill_in "Precipitation", with: 3
-    click_on "Create"
+    # fill_in "condition[date]",  with: "1/1/11"
+    fill_in "condition[max_temp]", with: 100.0
+    fill_in "condition[min_temp]", with: 22.0
+    fill_in "condition[mean_temp]", with: (100+22)/2.0
+    fill_in "condition[mean_humidity]", with: 30.0
+    fill_in "condition[mean_visibility]", with: 3.0
+    fill_in "condition[mean_wind]", with: 35.0
+    fill_in "condition[precipitation]", with: 3.0
+    click_on "Create Condition"
 
     condition = Condition.last
 
-    expect(current_path).to eq("/condition/#{condition.id}")
-
-    expect(page).to have_button("Add a Condition")
+    expect(current_path).to eq("/conditions/#{condition.id}")
+    # expect(page).to have_content("Date: #{condition.date}")
+    expect(page).to have_content("Precipitation: #{condition.precipitation}")
+    expect(page).to have_content("Mean Temperature: #{condition.mean_temp}")
+    # expect(page).to have_button("Delete this Condition")
   end
 end
